e5f823a7cb7ab0027c5f89978b9efbb863d32338
TheVeiler <duboisvalentin1010@gmail.com>
2026-01-27 00:20:56 +0100

    Setup files for publication

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { Card, CardStorage, Decklist } from "lib";
 import * as plugins from "plugins";
 
-export default { Card, CardStorage, Decklist, plugins };
+export { Card, CardStorage, Decklist, plugins };
