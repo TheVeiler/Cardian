b38e683db37bcd49faa9c3b965a9edff855e3d52
TheVeiler <duboisvalentin1010@gmail.com>
2026-01-27 23:21:52 +0100

    added default export

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import { Card, CardStorage, Decklist } from "lib";
 import * as plugins from "plugins";
 
 export { Card, CardStorage, Decklist, plugins };
+export default { Card, CardStorage, Decklist, plugins };
