3b01a925c54404fd9719ea6666d43b524ade2df1
TheVeiler <duboisvalentin1010@gmail.com>
2026-03-03 22:44:54 +0100

    Added sextet_standard52 as a plugin decklist

--- src/plugins/decklists/index.ts
+++ src/plugins/decklists/index.ts
@@ -1,1 +1,2 @@
 export { default as standard52 } from "./standard52/";
+export { default as sextet_standard52 } from "./sextet_standard52/";
---
+++ src/plugins/decklists/sextet_standard52/index.ts
@@ -0,0 +1,54 @@
+import { PseudoCard } from "lib";
+
+const rootDirectory = import.meta.url.split("node_modules/cardian/")[0];
+const assetsDirectory = `${rootDirectory}public/plugins/decklists/standard52/`;
+
+const ranks = new Set(["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]);
+const suits = new Set(["♦", "♠", "♥", "♣"]);
+
+const suitInitials = {
+	"♦": "d",
+	"♠": "s",
+	"♥": "h",
+	"♣": "c",
+};
+
+/**
+ * Six standard 52-card decks of French-suited playing cards (Ace to King, for each of the four suits: clubs, diamonds, hearts and spades).
+ */
+const pseudoCards: Array<PseudoCard> = [];
+
+for (const suit of suits) {
+	for (const rank of ranks) {
+		const filename = `${rank.toLowerCase()}${suitInitials[suit]}`;
+		const pseudoCard = {
+			name: `${rank}${suit}`,
+			assets: {
+				front: new URL(`${filename}.svg`, assetsDirectory),
+				back: new URL("back_blue.svg", assetsDirectory),
+			},
+			copies: 6,
+		};
+
+		pseudoCards.push(pseudoCard);
+	}
+}
+
+export default pseudoCards;
+
+export type CardValue =
+	| "A"
+	| "K"
+	| "Q"
+	| "J"
+	| "10"
+	| "9"
+	| "8"
+	| "7"
+	| "6"
+	| "5"
+	| "4"
+	| "3"
+	| "2";
+export type CardSuit = "♣" | "♦" | "♥" | "♠";
+export type CardName = `${CardValue}${CardSuit}`;
