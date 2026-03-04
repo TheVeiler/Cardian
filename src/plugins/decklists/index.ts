75b8587e946202b622d157777921d6cde426affe
TheVeiler <duboisvalentin1010@gmail.com>
2026-03-04 04:39:36 +0100

    renamed sextet_standard52 to standard52_sextet

--- src/plugins/decklists/index.ts
+++ src/plugins/decklists/index.ts
@@ -1,2 +1,2 @@
 export { default as standard52 } from "./standard52/";
-export { default as sextet_standard52 } from "./sextet_standard52/";
+export { default as standard52_sextet } from "./standard52_sextet";
